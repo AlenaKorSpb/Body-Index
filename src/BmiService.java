2182586acf3ac92a6a723396c767d468e03bf8b2
AlenaKorSpb <evtam-29@mail.ru>
2020-09-17 23:29:07 +0300

    change commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
-    public double calculate (int weight, int growth){
+    public double calculate(int weight, int growth) {
         double index = 10000.0 * weight / growth / growth;
         return index;
     }
